17793f7a73b9bdc8d8d06a4910adfa60c9092cdf
Damned Angel <danilo@angelo.eng.br>
2020-11-15 10:34:30 -0300

    [TKT_0001] Project Created.

---
+++ Snake-Fallen-in-Eden/targetconfig.h
@@ -0,0 +1,15 @@
+//-------------------------------------------------	
+// targetconfig.h created automatically by makefile	
+// on  8:55:32.25 , 14-Nov-20 				
+//														
+// DO NOT BOTHER EDITING THIS.							
+// ALL CHANGES WILL BE LOST.							
+//-------------------------------------------------	
+														
+#ifndef  __TARGETCONFIG_H__							
+#define  __TARGETCONFIG_H__							
+														
+#define DEBUG 1							
+#define OPENMSX_PROFILING								
+														
+#endif	//  __TARGETCONFIG_H__							
---
+++ Snake-Fallen-in-Eden/targetconfig.s
@@ -0,0 +1,10 @@
+;-------------------------------------------------		
+; targetconfig.s created automatically by makefile		
+; on  8:55:32.25 , 14-Nov-20 				
+;														
+; DO NOT BOTHER EDITING THIS.							
+; ALL CHANGES WILL BE LOST.							
+;-------------------------------------------------		
+														
+DEBUG = 1									
+OPENMSX_PROFILING = 1									
