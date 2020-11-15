9e1297e7df57eaaa6dff9d6078cfe50e4d12ef4c
Damned Angel <danilo@angelo.eng.br>
2020-11-15 17:56:37 -0300

    [TKT_0004] Bug fixed, documentation updated.

--- Snake-Fallen-in-Eden/targetconfig.h
+++ Snake-Fallen-in-Eden/targetconfig.h
@@ -1,6 +1,6 @@
 //-------------------------------------------------	
 // targetconfig.h created automatically by makefile	
-// on 16:05:09.12 , 15-Nov-20 				
+// on 16:34:57.02 , 15-Nov-20 				
 //														
 // DO NOT BOTHER EDITING THIS.							
 // ALL CHANGES WILL BE LOST.							
