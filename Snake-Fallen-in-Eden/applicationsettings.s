256f21b405f1bf8f80dd5da880d417601e383011
Damned Angel <danilo@angelo.eng.br>
2021-07-08 08:34:16 -0300

    [TKT0046] Head turning.

--- Snake-Fallen-in-Eden/applicationsettings.s
+++ Snake-Fallen-in-Eden/applicationsettings.s
@@ -1,7 +1,7 @@
 ;-------------------------------------------------		
 ; applicationsettings.s created automatically			
 ; by make.bat											
-; on  8:21:51.39 , 08-Jul-21 				
+; on  8:28:31.33 , 08-Jul-21 				
 ;														
 ; DO NOT BOTHER EDITING THIS.							
 ; ALL CHANGES WILL BE LOST.							
