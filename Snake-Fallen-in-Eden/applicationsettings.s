faf56d991f34b0916b3e34d3e1490026e9171a7b
Damned Angel <danilo@angelo.eng.br>
2021-07-15 07:05:32 -0300

    [TKT0047] Colision tile shown on impact. Animation not implemented yet.

--- Snake-Fallen-in-Eden/applicationsettings.s
+++ Snake-Fallen-in-Eden/applicationsettings.s
@@ -1,7 +1,7 @@
 ;-------------------------------------------------		
 ; applicationsettings.s created automatically			
 ; by make.bat											
-; on  8:28:31.33 , 08-Jul-21 				
+; on  0:20:33.82 , 09-Jul-21 				
 ;														
 ; DO NOT BOTHER EDITING THIS.							
 ; ALL CHANGES WILL BE LOST.							
